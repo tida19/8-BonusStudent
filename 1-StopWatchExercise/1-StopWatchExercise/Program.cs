5ac1d8bef37eca6143a0aab33b4a36784b9c1d3e
tida bonguleaum <56539596+tida19@users.noreply.github.com>
2019-10-17 17:00:18 +0700

    1-StopWatchExercise

---
+++ 1-StopWatchExercise/1-StopWatchExercise/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace _1_StopWatchExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new StopWatch();
+
+            for (var i = 0; i < 2; i++)
+            {
+                stopwatch.Start();
+                Thread.Sleep(1000);
+
+                //Stopwatch.Start(DataTime.Now);
+
+                stopwatch.Stop();
+
+                Console.WriteLine("Duration: " + stopwatch.GetInterval());
+
+                Console.WriteLine("Press Enter to run the stopwatch one more time.");
+                Console.ReadLine();
+            }
+        }
+    }
+    public class StopWatch
+    {
+        private DateTime _startTime;
+        private DateTime _endTime;
+        private bool _running;
+        public void Start()
+        {
+            if (_running)
+
+                throw new IndexOutOfRangeException("Stopwatch is already running!");
+
+            _startTime = DateTime.Now;
+            _running = true;
+
+        }
+        public void Stop()
+        {
+            if (!_running)
+                throw new IndexOutOfRangeException("Stopwatch is already running!");
+
+            _endTime = DateTime.Now;
+            _running = false;
+
+        }
+        public TimeSpan GetInterval()
+        {
+            return _endTime - _startTime;
+        }
+    }
+}
